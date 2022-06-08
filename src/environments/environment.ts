7367629237504e9f591c1abc5d80e1de0e3978f6
Dinakaran97 <appudina4@gmail.com>
2022-06-08 21:49:46 +0530

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyB8BBLb1s2oJJ1Kb2_S90Ov84Uha-tgHsg",
-    authDomain: "one-time-ca4e5.firebaseapp.com",
-    projectId: "one-time-ca4e5",
-    storageBucket: "one-time-ca4e5.appspot.com",
-    messagingSenderId: "154217750369",
-    appId: "1:154217750369:web:7a193b769a0616bf82ef20",
-    measurementId: "G-DN8596WVQS"
+    apiKey: "AIzaSyCUoqTKD6ppErzTszJDB6wtx1_k6QwRXkU",
+    authDomain: "imgupload-9b9cd.firebaseapp.com",
+    databaseURL: "https://imgupload-9b9cd-default-rtdb.firebaseio.com",
+    projectId: "imgupload-9b9cd",
+    storageBucket: "imgupload-9b9cd.appspot.com",
+    messagingSenderId: "156671026217",
+    appId: "1:156671026217:web:9d83ba5efcc832a89fe838"
   }
 };
